6b703c2d9f67e54f0bc1b8f8316ce1104329bf5d
elvysbatzibal <ebatzibal@plusti.com>
2025-05-16 23:11:14 -0600

    Agregar archivos de configuración de Docker, esquemas de base de datos y datos iniciales para el proyecto

---
+++ 00schemadb/seed.sql
@@ -0,0 +1,44 @@
+INSERT INTO Products (name, category, description, specs)
+VALUES 
+(
+  'ANC-35',
+  'Truper',
+  'Blister con 10 anclajes para redes de carga de 3.5 ton.',
+  '{"code": 103597, "availability": "Disponible"}'
+);
+
+INSERT INTO Products (name, category, description, specs)
+VALUES 
+(
+  'DIA-250AE',
+  'Truper',
+  'Diablo de carga de aluminio 250 kg con extensión de base',
+  '{"code": 103575, "availability": "Disponible"}'
+);
+
+INSERT INTO Products (name, category, description, specs)
+VALUES 
+(
+  'MAX-20SCS',
+  'Truper',
+  'Sierra circular 6-1/2", 20V s/accesorios, MAX',
+  '{"code": 103234, "availability": "Disponible"}'
+);
+
+INSERT INTO Products (name, category, description, specs)
+VALUES 
+(
+  'SICI-20AS',
+  'Truper',
+  'Sierra circular 6-1/2", 20V, sin batería 4Ah, PRO',
+  '{"code": 103242, "availability": "Disponible"}'
+);
+
+INSERT INTO Products (name, category, description, specs)
+VALUES 
+(
+  'WT-30',
+  'Truper',
+  'Aceite multiusos en aerosol, 297ml (10oz), Truper',
+  '{"code": 103697, "availability": "Disponible"}'
+);
